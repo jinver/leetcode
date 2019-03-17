21c8e15b503413f59fd9d58d905f90789820e94a
Jin Zhang <jinverr@gmail.com>
2019-03-17 00:03:23 -0400

    coin change problem

---
+++ leetcode/lc98-validate-binary-search-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, val):
+        pass
+
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        
+    
